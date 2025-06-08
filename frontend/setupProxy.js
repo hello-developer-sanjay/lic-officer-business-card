2dcae4458d869500dfcd94f8a212b174cdbf1129
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-08 19:15:04 +0530

    Update setupProxy.js

--- frontend/setupProxy.js
+++ frontend/setupProxy.js
@@ -1,11 +1,1 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://eduxcel-api-13april.onrender.com/',
-      changeOrigin: true,
-    })
-  );
-};
+/*add proxy when needed*/
