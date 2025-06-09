51c2677af53500f64f1a8d566c28212670732c3d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-09 23:01:22 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export uvedefault defineConfig({
+export default defineConfig({
   plugins: [
     react({
       jsxRuntime: 'automatic',
