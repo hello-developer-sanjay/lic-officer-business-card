3f797b83d4c4878dbe98f66ed9834bf40b6903ea
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-10 01:24:04 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -26,11 +26,6 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/': {
-        target: 'https://lic-backend-8jun.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path,
-      },
       '/api': {
         target: 'https://lic-backend-8jun.onrender.com',
         changeOrigin: true,
