470e8e8aa1bb42f45aa55a9af6236c59aad53a7a
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-06 20:47:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-                  <Route path="/" element={<LicCaseStudy/>} />
+                  <Route path="/" element={<homePage/>} />
 
         <Route path="/reviews" element={<Reviews />} />
         
