e447b3bc51efc3df747db4730044bd7c6cb0c38d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-06 20:44:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import homePage from './pages/homePage';
 import Reviews from './pages/Reviews';
+import LicCaseStudy from './pages/LicCaseStudy';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+                  <Route path="/" element={<LicCaseStudy/>} />
+
         <Route path="/reviews" element={<Reviews />} />
+        
         <Route path="/join" element={<div>Join Page (TBD)</div>} />
         <Route path="/services" element={<div>Services Page (TBD)</div>} />
         <Route path="/about" element={<div>About Page (TBD)</div>} />
