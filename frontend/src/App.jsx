04805df6c55e3ec0d907a912a13d3037d23c8145
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-07 21:37:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LicCaseStudy from './pages/LicCaseStudy';
+import LicServices from './pages/LicServices';
 
-const App = () => <LicCaseStudy />;
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LicCaseStudy />} />
+      <Route path="/services" element={<LicServices />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
