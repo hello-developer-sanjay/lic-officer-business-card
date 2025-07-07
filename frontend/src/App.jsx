2189222577606b60409efc48feabd826f0543a82
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-07 23:57:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,20 @@ import LicServices from './pages/LicServices';
 import LicJeevenAnand from './pages/LicJeevenAnand';
 import NewJeevanShanti from './pages/NewJeevanShanti';
 import ChildMoneyBack from './pages/ChildMoneyBack';
+import JeevanLabh from './pages/JeevanLabh';
 
 import HealthProtectionPlus from './pages/HealthProtectionPlus';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<LicCaseStudy />} />
-      <Route path="/services" element={<LicServices />} />
-            <Route path="/policies/jeevan-anand" element={<LicJeevenAnand />} />
-            <Route path="/policies/jeevan-shanti" element={<NewJeevanShanti />} />
-            <Route path="/policies/health-protection-plus" element={<HealthProtectionPlus />} />
-            <Route path="/policies/child-money-back" element={<ChildMoneyBack />} />
+      <Route path="/" element={<LicCaseStudy/>} />
+      <Route path="/services" element={<LicServices/>} />
+            <Route path="/policies/jeevan-anand" element={<LicJeevenAnand/>} />
+            <Route path="/policies/jeevan-shanti" element={<NewJeevanShanti/>} />
+            <Route path="/policies/health-protection-plus" element={<HealthProtectionPlus/>} />
+            <Route path="/policies/child-money-back" element={<ChildMoneyBack/>} />
+            <Route path="/policies/jeevan-labh" element={<JeevanLabh/>} />
 
       
 
