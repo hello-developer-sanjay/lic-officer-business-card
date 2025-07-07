fc6d50ef087603d7b7a9c53af4b25687e84bb518
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-07-07 22:15:45 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LicCaseStudy from './pages/LicCaseStudy';
 import LicServices from './pages/LicServices';
+import LicJeevenAnand from './pages/LicJeevenAnand';
+
+import HealthProtectionPlus from './pages/HealthProtectionPlus';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<LicCaseStudy />} />
       <Route path="/services" element={<LicServices />} />
+            <Route path="/policies/jeevan-anand" element={<LicJeevenAnand />} />
+
+            <Route path="/policies/health-protection-plus" element={<HealthProtectionPlus />} />
+
     </Routes>
   </Router>
 );
